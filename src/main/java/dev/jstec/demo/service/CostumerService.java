1b4cbceb1942a2bb77de869dd656d045d6d71127
Jorge Nascimento <jorge.donascimento@agricultura.gov.br>
2023-03-30 21:01:50 -0300

    Alterado o contexto da api

--- src/main/java/dev/jstec/demo/service/CostumerService.java
+++ src/main/java/dev/jstec/demo/service/CostumerService.java
@@ -6,7 +6,6 @@ import dev.jstec.demo.resource.dto.CostumerDTO;
 import dev.jstec.demo.resource.dto.CostumerListDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
