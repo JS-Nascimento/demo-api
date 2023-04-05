eabec48a7f08a92ae1deeeda0603e6a4cec55fe7
jorge.donascimento <jorge.donascimento@contratado.agricultura.gov.br>
2023-04-04 22:33:22 -0300

    Feat - Amazon S3 features

--- src/main/java/dev/jstec/demo/resource/controller/AmazonS3Controller.java
+++ src/main/java/dev/jstec/demo/resource/controller/AmazonS3Controller.java
@@ -1,14 +1,27 @@
 package dev.jstec.demo.resource.controller;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import dev.jstec.demo.service.AmazonS3Service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @RestController
@@ -60,6 +73,33 @@ public class AmazonS3Controller {
             throw new Exception("Erro desconhecido");
         }
 
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<InputStreamResource> downloadFromS3(@RequestParam String bucketName,
+                                                              @RequestParam String objectName,
+                                                              @RequestParam String downloadPath) {
+
+        AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
+
+        try {
+            S3Object s3object = s3client.getObject(new GetObjectRequest(bucketName, objectName));
+            S3ObjectInputStream inputStream = s3object.getObjectContent();
+            File downloadedFile = new File(downloadPath);
+
+            FileUtils.copyInputStreamToFile(inputStream, downloadedFile);
+
+            return ResponseEntity.ok()
+                    .contentLength(downloadedFile.length())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + downloadedFile.getName() + "\"")
+                    .body(new InputStreamResource(new FileInputStream(downloadedFile)));
+
+        } catch (AmazonServiceException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
